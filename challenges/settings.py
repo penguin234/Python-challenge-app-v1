9d58c7ad91803cf1eb3dfd97ef2713699bf352e2
JaeHyun Hwang <hjh6195@naver.com>
2020-04-13 21:17:51 +0900

    fixing settings.py for heroku push rejection

--- challenges/settings.py
+++ challenges/settings.py
@@ -119,6 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 
 
